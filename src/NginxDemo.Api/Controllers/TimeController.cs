72f874745ef3303c1a3a705891d4b1e94fc7ac5a
yangww <dayang_techmail@163.com>
2020-10-30 15:46:36 +0800

    init

---
+++ src/NginxDemo.Api/Controllers/TimeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NginxDemo.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        public TimeController()
+        {
+        }
+
+        /// <summary>
+        /// 获取utc时间
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult> ZeroTimeAsync()
+        {
+            var time = DateTime.UtcNow;
+            return Ok(time);
+        }
+    }
+}
